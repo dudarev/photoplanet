47b1a0e812ee5cfe02ae2acf65b591d3748851aa
Artem Dudarev <dudarev@gmail.com>
2013-04-20 22:07:50 +0300

    24: all photos page is served from class based view

--- photoplanet/photoplanet/urls.py
+++ photoplanet/photoplanet/urls.py
@@ -1,12 +1,17 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
+urlpatterns = patterns(
+    '',
     url(r'^$', 'photoplanet.views.home', name='home'),
+    url(
+        r'^all/',
+        TemplateView.as_view(template_name="photoplanet/all.html"),
+        name='all')
     # url(r'^photoplanet/', include('photoplanet.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
