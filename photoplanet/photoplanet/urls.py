c3fa5dcaaf138c3b315d5daec15a12dda930e212
Artem Dudarev <dudarev@gmail.com>
2013-04-22 00:23:00 +0300

    9: temporary solution for serving static on heroku

--- photoplanet/photoplanet/urls.py
+++ photoplanet/photoplanet/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, patterns, url
 from django.views.generic import TemplateView
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -13,4 +14,13 @@ urlpatterns = patterns(
         TemplateView.as_view(template_name="photoplanet/all.html"),
         name='all'),
     url(r'^feedback/', include('feedback.urls'))
+)
+
+# http://stackoverflow.com/questions/9047054/heroku-handling-static-files-in-django-app
+# TODO: needs to be removed this is temporary to have static served from HEROKU
+urlpatterns += patterns(
+    '',
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.STATIC_ROOT,
+    }),
 )
