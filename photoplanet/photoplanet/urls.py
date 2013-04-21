80a5a4ab02280c10b9fd5f9716c62a04c76588ed
Artem Dudarev <dudarev@gmail.com>
2013-04-22 00:23:00 +0300

    9: trying for serving static on heroku

--- photoplanet/photoplanet/urls.py
+++ photoplanet/photoplanet/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, patterns, url
 from django.views.generic import TemplateView
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -13,4 +14,11 @@ urlpatterns = patterns(
         TemplateView.as_view(template_name="photoplanet/all.html"),
         name='all'),
     url(r'^feedback/', include('feedback.urls'))
+)
+
+urlpatterns += patterns(
+    '',
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.STATIC_ROOT,
+    }),
 )
