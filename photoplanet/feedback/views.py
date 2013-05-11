3ff3849866a313d30aa29f7b085a37d6246d7bc7
Artem Dudarev <dudarev@gmail.com>
2013-05-11 11:09:48 +0300

    42: feedback as CreateView with ModelForm

--- photoplanet/feedback/views.py
+++ photoplanet/feedback/views.py
@@ -1,5 +1,27 @@
-from django.views.generic import TemplateView
+from django import forms
+from django.views.generic import CreateView
+from django.core.urlresolvers import reverse_lazy
+
+from braces.views import LoginRequiredMixin
+
+from .models import Feedback
 
 
-class FeedbackView(TemplateView):
-    template_name = "feedback/feedback.html"
+class FeedbackForm(forms.ModelForm):
+    class Meta:
+        model = Feedback
+        exclude = ('user', )
+
+
+class FeedbackCreateView(LoginRequiredMixin, CreateView):
+    model = Feedback
+    form_class = FeedbackForm
+    template_name = 'feedback/feedback.html'
+    # https://docs.djangoproject.com/en/1.5/topics/class-based-views/generic-editing/#model-forms
+    success_url = reverse_lazy('home')
+    
+    # https://docs.djangoproject.com/en/1.5/topics/class-based-views/generic-editing/
+    # #models-and-request-user
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(FeedbackCreateView, self).form_valid(form)
