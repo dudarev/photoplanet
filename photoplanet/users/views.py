51275c6959ed6d27ad55065eed021b41deea4ca9
Artem Dudarev <dudarev@gmail.com>
2013-05-11 11:49:28 +0300

    added handling of next GET parameter during login

--- photoplanet/users/views.py
+++ photoplanet/users/views.py
@@ -4,6 +4,11 @@ from django.views.generic import TemplateView
 class LoginView(TemplateView):
     template_name = "users/login.html"
 
+    def get_context_data(self, **kwargs):
+        context = super(LoginView, self).get_context_data(**kwargs)
+        context['next'] = self.request.GET.get('next', None)
+        return context
+
 
 class LoginErrorView(TemplateView):
     template_name = "users/login-error.html"
