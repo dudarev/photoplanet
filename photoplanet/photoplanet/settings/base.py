9087d3fcc144bcccf2c8998d3c6c9f10bb974f9b
ak <ak@fe10M2.finfort.com>
2014-09-10 21:14:20 +0300

    Committer: ak <ak@fe10M2.finfort.com>
     On branch dev
    	modified:   photoplanet/manage.py
    	modified:   photoplanet/photoplanet/settings/base.py
    	modified:   photoplanet/photoplanet/wsgi.py

--- photoplanet/photoplanet/settings/base.py
+++ photoplanet/photoplanet/settings/base.py
@@ -34,11 +34,13 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': normpath(join(SITE_ROOT, 'photoplanet.db')), # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'photoplanet', # Or path to database file if using sqlite3.
+#        'NAME': normpath(join(SITE_ROOT, 'photoplanet.db')), # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
+        'USER': 'photoplanet',
+        'PASSWORD': 'photoplanet',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
