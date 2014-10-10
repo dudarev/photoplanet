0e4b7122ba80788ae8223d519e28880fda388af9
ak <ak@fe10M2.finfort.com>
2014-10-10 10:48:33 +0300

    Committer: ak <ak@fe10M2.finfort.com>
     On branch dev
    
    	new file:   photoplanet/photoplanet/settings/instagram.py

---
+++ photoplanet/photoplanet/settings/instagram.py
@@ -0,0 +1,5 @@
+# rename to instagram.py
+# get variables from http://instagram.com/developer/clients/register/
+
+INSTAGRAM_CLIENT_ID = '{{ instagram_client_id }}'
+INSTAGRAM_CLIENT_SECRET = '{{ instagram_client_secret }}'
